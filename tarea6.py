171b1503162fd24588e13cc324d6c828222ccd4a
pabixo <pablo.alatorre@alumnos.udg.mx>
2016-10-04 22:46:48 -0500

    Add files via upload

---
+++ tarea6.py
@@ -0,0 +1,12 @@
+#Alatorre Vaca Pablo ELias
+#Seminario de Base de Datos
+#Tarea 6
+class Alumno():
+    self.nombre = ""
+    self.edad = ""
+    self.carrera = ""
+
+    def __init__(self):
+        self.nombre = "Pablo"
+        self.edad = "20"
+        self.carrera = "computacion"
